815471cbb30cd9b84f7aa9682739bb0250e65e69
PedroGM80 <pedro13087@gmail.com>
2022-10-13 14:07:23 +0200

    make StudentListActivity.kt and Xml files

--- app/src/main/java/dev/pgm/practicealten322/StudentListActivity.kt
+++ app/src/main/java/dev/pgm/practicealten322/StudentListActivity.kt
@@ -1,23 +1,33 @@
 package dev.pgm.practicealten322
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.utils.widget.MockView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import dev.pgm.practicealten322.databinding.ActivityStudentListBinding
+import dev.pgm.practicealten322.utils.StudentAdapter
+
 
 class StudentListActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityStudentListBinding
 
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityStudentListBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
@@ -30,6 +40,10 @@ class StudentListActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        val myRecyclerView:RecyclerView=findViewById(R.id.recycler_view_Students)
+        myRecyclerView.layoutManager=LinearLayoutManager(this)
+        val studentList=MockUtils.generateStudentList()
+        myRecyclerView.adapter=StudentAdapter(studentList)
     }
 
     override fun onSupportNavigateUp(): Boolean {
