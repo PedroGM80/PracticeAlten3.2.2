30075e1fb2ce453c8c4dd53d22625405bd28a021
PedroGM80 <pedro13087@gmail.com>
2022-10-13 09:42:37 +0200

    add StudentDomain.kt

--- app/src/main/java/dev/pgm/practicealten322/DataStudent.kt
+++
@@ -1,3 +0,0 @@
-package dev.pgm.practicealten322
-
-data class DataStudent(val studentReference:Int, val FullName:String, val note :String, val PhotoReference:Int, val course:String)
---
+++ app/src/main/java/dev/pgm/practicealten322/StudentDomain.kt
@@ -0,0 +1,3 @@
+package dev.pgm.practicealten322
+
+data class StudentDomain(val studentReference:Int, val FullName:String, val note :String, val PhotoReference:Int, val course:String)
