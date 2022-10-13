70366fe0eb850139cfcb52c87f854d814f0903ee
PedroGM80 <pedro13087@gmail.com>
2022-10-13 14:05:27 +0200

    update MockUtils.kt ans StudentAdapter.kt and StudentDomain.kt

--- app/src/main/java/dev/pgm/practicealten322/utils/StudentAdapter.kt
+++ app/src/main/java/dev/pgm/practicealten322/utils/StudentAdapter.kt
@@ -1,39 +1,27 @@
 package dev.pgm.practicealten322.utils
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.annotation.IdRes
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import dev.pgm.practicealten322.StudentDomain
 
-class BaseViewHolder(container: ViewGroup) : RecyclerView.ViewHolder(container)
+class StudentAdapter(val items: List<StudentDomain>) :
+    RecyclerView.Adapter<StudentAdapter.ViewHolder?>() {
 
-class BaseItemCallback<student : StudentDomain> : DiffUtil.ItemCallback<student>() {
-    override fun areItemsTheSame(oldItem: student, newItem: student) =
-        oldItem.toString() == newItem.toString()
-
-    override fun areContentsTheSame(oldItem: student, newItem: student) = oldItem == newItem
-}
-
-
-abstract class StudentAdapter(
-    @IdRes val layoutId: Int,
-    inline val bind: (item: StudentDomain, holder: BaseViewHolder, itemCount: Int) -> Unit,
-) : ListAdapter<StudentDomain, BaseViewHolder>(BaseItemCallback<StudentDomain>()) {
-    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-        bind(getItem(position), holder, itemCount)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+     return  ViewHolder(TextView(parent.context))
     }
 
-    override fun getItemViewType(position: Int) = layoutId
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-        val root = LayoutInflater.from(parent.context).inflate(
-            viewType, parent, false
-        )
-        return BaseViewHolder(root as ViewGroup)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = """${items[position].studentReference}
+            |${items[position].fullName}
+            |   ${items[position].age}
+            |   ${items[position].course}
+            |   ${items[position].note}
+            | 
+            |""".trimMargin()
     }
 
-    override fun getItemCount() = currentList.size
-
+    override fun getItemCount(): Int = items.size
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
